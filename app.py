9ae1129d28db4387da6f967b6f4994cb7a6b7f87
rishysp <58654138+rishysp@users.noreply.github.com>
2022-05-17 00:11:58 +0530

    Final

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from typing import final
+from flask import Flask,render_template,request
 import cv2
-from keras.applications import resnet
+from keras.applications.resnet import ResNet50
+import numpy as np
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
@@ -16,132 +18,93 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense, Flatten,Input, Convolution2D, Dropout, LSTM, TimeDistributed, Embedding, Bidirectional, Activation, RepeatVector,Concatenate
 from keras.models import Sequential, Model
 from tqdm import tqdm
-import numpy as np
 
-vocab = np.load('vocab.npy', allow_pickle=True)
-
+vocab = np.load('vocab.npy',allow_pickle=True)
 vocab = vocab.item()
-
 inv_vocab = {v:k for k,v in vocab.items()}
+resnet = ResNet50(include_top = False,  weights = 'imagenet',input_shape = (224,224,3), pooling = 'avg')
 
-
-print("+"*50)
-print("vocabulary loaded")
-
+print("*"*50)
+print("resnet loaded")
 
 embedding_size = 128
-vocab_size = len(vocab)
 max_len = 40
-
-
+vocab_size = len(vocab) + 1
 image_model = Sequential()
-
-image_model.add(Dense(embedding_size, input_shape=(2048,), activation='relu'))
+image_model.add(Dense(embedding_size,input_shape = (2048,),activation = 'relu'))
 image_model.add(RepeatVector(max_len))
 
-
 language_model = Sequential()
-
-language_model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size, input_length=max_len))
-language_model.add(LSTM(256, return_sequences=True))
+language_model.add(Embedding(input_dim = vocab_size,output_dim = embedding_size,input_length = max_len))
+language_model.add(LSTM(256,return_sequences = True))
 language_model.add(TimeDistributed(Dense(embedding_size)))
 
+conca = Concatenate()([image_model.output,language_model.output])
 
-conca = Concatenate()([image_model.output, language_model.output])
-x = LSTM(128, return_sequences=True)(conca)
-x = LSTM(512, return_sequences=False)(x)
+x = LSTM(128,return_sequences = True)(conca)
+x = LSTM(512,return_sequences = False)(x)
 x = Dense(vocab_size)(x)
+
 out = Activation('softmax')(x)
-model = Model(inputs=[image_model.input, language_model.input], outputs = out)
 
-model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
-
-model.load_weights('mine_model_weights.h5')
-
-print("="*150)
-print("MODEL LOADED")
-
-resnet = resnet.ResNet50(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
-
-
-#resnet = load_model('resnet.h5')
-
-print("="*150)
-print("RESNET MODEL LOADED")
-
-
+model = Model(inputs = [image_model.input,language_model.input],outputs = out)
+model.compile(loss = 'categorical_crossentropy',optimizer = 'RMSProp',metrics = ['accuracy'])
 
 
 app = Flask(__name__)
-
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
+model.load_weights('mine_model_weights.h5')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/after', methods=['GET', 'POST'])
+@app.route('/after',methods = ['GET','POST'])
 def after():
-
-    global model, resnet, vocab, inv_vocab
-
-    img = request.files['file1']
-
-    img.save('static/file.jpg')
-
-    print("="*50)
-    print("IMAGE SAVED")
-
-
+    global model,vocab,inv_vocab
     
-    image = cv2.imread('static/file.jpg')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    image = cv2.resize(image, (224,224))
-
-    image = np.reshape(image, (1,224,224,3))
-
+    print("*"*50)
+    print("model loaded")
     
+    file = request.files['file1']
+    file.save('static/file.jpg')
     
-    incept = resnet.predict(image).reshape(1,2048)
-
-    print("="*50)
-    print("Predict Features")
-
-
-    text_in = ['startofseq']
-
-    final = ''
-
-    print("="*50)
-    print("GETING Captions")
+    img = cv2.imread('static/file.jpg')
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img,(224,224))
+    img = img.reshape(1,224,224,3)
+    
+    features = resnet.predict(img).reshape(1,2048)
+    
+    text_inp = ['startofseq']
 
     count = 0
-    while tqdm(count < 20):
-
+    caption = ''
+    while count < 25:
         count += 1
 
         encoded = []
-        for i in text_in:
+        for i in text_inp:
             encoded.append(vocab[i])
 
-        padded = pad_sequences([encoded], maxlen=max_len, padding='post', truncating='post').reshape(1,max_len)
+        encoded = [encoded]
 
-        sampled_index = np.argmax(model.predict([incept, padded]))
-
-        sampled_word = inv_vocab[sampled_index]
-
-        if sampled_word != 'endofseq':
-            final = final + ' ' + sampled_word
-
-        text_in.append(sampled_word)
+        encoded = pad_sequences(encoded, padding='post', truncating='post', maxlen=max_len)
 
 
+        prediction = np.argmax(model.predict([features, encoded]))
 
-    return render_template('after.html', data=final)
+        sampled_word = inv_vocab[prediction]
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        caption = caption + ' ' + sampled_word
+            
+        if sampled_word == 'endofseq':
+            break
 
+        text_inp.append(sampled_word)
+    
+    return render_template('after.html',final = caption)    
 
+if __name__ == '__main__':
+    app.run()
